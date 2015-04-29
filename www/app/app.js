37a388e4e9e24cb81c17f1d58cef5e7abf4437c2
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-04-29 15:59:51 +0100

    whoops, getting ahead of myself

--- www/app/app.js
+++ www/app/app.js
@@ -29,5 +29,5 @@ angular.module('eliteApp', ['ionic'])
       });
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/home')
+    $urlRouterProvider.otherwise('/app')
   });
