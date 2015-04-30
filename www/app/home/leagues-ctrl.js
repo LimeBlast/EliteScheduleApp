5e171f8281dd2830ec70e3cf22730a1273136274
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-04-30 11:46:00 +0100

    added some hard-coded league data, a service to access it, and a leagues controller

---
+++ www/app/home/leagues-ctrl.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular.module('eliteApp').controller('LeaguesCtrl', ['eliteApi', LeaguesCtrl]);
+
+  function LeaguesCtrl(eliteApi) {
+    var vm = this;
+
+    var leagues = eliteApi.getLeagues();
+    var leagueData = eliteApi.getLeagueData();
+
+    console.log(leagues, leagueData);
+
+  }
+})();
