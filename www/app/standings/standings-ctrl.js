141c6c9683fc65f8dec122b82986375f9a93b8e0
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-04-30 13:27:57 +0100

    started teams and standings screens

---
+++ www/app/standings/standings-ctrl.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  angular.module('eliteApp').controller('StandingsCtrl', ['eliteApi', StandingsCtrl]);
+
+  function StandingsCtrl(eliteApi) {
+    var vm = this;
+
+    var data = eliteApi.getLeagueData();
+    vm.standings = data.standings;
+  }
+})();
