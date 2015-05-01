7bc0a20d18b7fe5bf9f05e3e8064167577c89633
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-05-01 13:54:07 +0100

    added another service to track my teams

--- www/app/teams/team-detail-ctrl.js
+++ www/app/teams/team-detail-ctrl.js
@@ -1,47 +1,51 @@
 (function () {
   'use strict';
 
-  angular.module('eliteApp').controller('TeamDetailCtrl', ['$stateParams', '$ionicPopup', 'eliteApi', TeamDetailCtrl]);
+  angular.module('eliteApp').controller('TeamDetailCtrl', ['$stateParams', '$ionicPopup', 'eliteApi', 'myTeamsService', TeamDetailCtrl]);
 
-  function TeamDetailCtrl($stateParams, $ionicPopup, eliteApi) {
-    var vm = this;
+  function TeamDetailCtrl($stateParams, $ionicPopup, eliteApi, myTeamsService) {
+    var vm = this,
+      team = null,
+      leagueData = null;
 
+    console.log("$stateParams", $stateParams);
     vm.teamId = Number($stateParams.id);
 
-    eliteApi.getLeagueData()
-      .then(function (data) {
-        var team = _.chain(data.teams)
-          .flatten("divisionTeams")
-          .find({"id": vm.teamId})
-          .value();
+    eliteApi.getLeagueData().then(function (data) {
+      team = _.chain(data.teams)
+        .flatten("divisionTeams")
+        .find({"id": vm.teamId})
+        .value();
 
-        vm.teamName = team.name;
+      vm.teamName = team.name;
 
-        vm.games = _.chain(data.games)
-          .filter(isTeamInGame)
-          .map(function (item) {
-            var isTeam1 = (item.team1Id === vm.teamId ? true : false);
-            var opponentName = isTeam1 ? item.team2 : item.team1;
-            var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
-            return {
-              gameId: item.id,
-              opponent: opponentName,
-              time: item.time,
-              location: item.location,
-              locationUrl: item.locationUrl,
-              scoreDisplay: scoreDisplay,
-              homeAway: (isTeam1 ? "vs." : "at")
-            };
-          })
-          .value();
+      vm.games = _.chain(data.games)
+        .filter(isTeamInGame)
+        .map(function (item) {
+          var isTeam1 = (item.team1Id === vm.teamId ? true : false);
+          var opponentName = isTeam1 ? item.team2 : item.team1;
+          var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
+          return {
+            gameId: item.id,
+            opponent: opponentName,
+            time: item.time,
+            location: item.location,
+            locationUrl: item.locationUrl,
+            scoreDisplay: scoreDisplay,
+            homeAway: (isTeam1 ? "vs." : "at")
+          };
+        })
+        .value();
 
-        vm.teamStanding = _.chain(data.standings)
-          .flatten("divisionStandings")
-          .find({"teamId": vm.teamId})
-          .value();
+      vm.teamStanding = _.chain(data.standings)
+        .flatten("divisionStandings")
+        .find({"teamId": vm.teamId})
+        .value();
 
-        vm.following = false;
-      });
+      leagueData = data.league;
+    });
+
+    vm.following = myTeamsService.isFollowingTeam(vm.teamId.toString());
 
     vm.toggleFollow = function () {
 
@@ -53,10 +57,12 @@
         confirmPopup.then(function (res) {
           if (res) {
             vm.following = !vm.following;
+            myTeamsService.unfollowTeam(team.id);
           }
         });
       } else {
         vm.following = !vm.following;
+        myTeamsService.followTeam({id: team.id, name: team.name, leagueId: leagueData.id, leagueName: leagueData.name});
       }
     };
 
